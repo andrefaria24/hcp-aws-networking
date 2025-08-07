9e5fd3bb9bd0e57abbf5c22d601610bff37a8a78
Andre Faria <andre.faria.24@gmail.com>
2025-08-07 14:59:22 -0400

    corrected subnet cidrs

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ module "vpc" {
   cidr = "10.124.0.0/16"
 
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.124.1.0/8", "10.124.2.0/8", "10.124.3.0/8"]
-  public_subnets  = ["10.124.11.0/8", "10.124.12.0/8", "10.124.13.0/8"]
+  private_subnets = ["10.124.1.0/24", "10.124.2.0/24", "10.124.3.0/24"]
+  public_subnets  = ["10.124.11.0/24", "10.124.12.0/24", "10.124.13.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = false
