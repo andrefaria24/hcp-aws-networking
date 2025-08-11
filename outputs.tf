91b077ba3ffda527a2d0e3af691caa15a05a935e
Andre Faria <andre.faria.24@gmail.com>
2025-08-11 11:48:40 -0400

    added db_subnet_group output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "private_subnets" {
   value       = module.vpc.private_subnets
   description = "Private VPC subnets"
 }
+
+output "db_subnet_group" {
+  value = aws_db_subnet_group.default
+  description = "Default database subnet group"
+}
