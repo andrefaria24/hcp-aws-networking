38fec2639bb5af004273bc886cee271f9e6cdbd9
Andre Faria <andre.faria.24@gmail.com>
2025-08-20 10:28:34 -0400

    added security_group_id outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "db_subnet_group_name" {
   value       = aws_db_subnet_group.db.name
   description = "Default database subnet group"
 }
+
+output "psql_security_group_id" {
+  value       = aws_security_group.postgresql.id
+  description = "Postgresql security group id"
+}
+
+output "mysql_security_group_id" {
+  value       = aws_security_group.mysql.id
+  description = "MySQL security group id"
+}
